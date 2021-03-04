5efc639ee8e16e3d6008188151d30809733f9846
Andrew Shenstone <shenst1@gmail.com>
2021-03-04 11:56:18 -0800

    Update section component for flexibility.

--- components/widget-section.js
+++ components/widget-section.js
@@ -1,12 +1,12 @@
-import { Image } from 'react-datocms'
-export default function WidgetSection({widget: {header, body }}) {
+import {useRouter} from 'next/router'
+export default function WidgetSection({widget: {header, body, width, showBackButton }}) {
+  const router = useRouter();
   return (
     <div className="uk-section">
-      <div className="uk-container">
-        <div className=" uk-text-center">
-          {header && <h1 className='uk-margin-medium-bottom'>{header}</h1>}
-          {body && <div dangerouslySetInnerHTML={{__html: body }} />}
-        </div>
+      <div className={ `uk-container ${width !== "medium" && `uk-container-${width}`}`}>
+          { header && <h1 className='uk-margin-medium-bottom'>{header}</h1> }
+          { showBackButton &&  <a onClick={() => router.back()} className="nov-button-icon-link uk-flex uk-flex-inline uk-flex-middle nov-text-semibold uk-margin-bottom"><i class="icomoon-arrow-left-circle uk-margin-remove-left "></i><span class="uk-text-uppercase uk-text-small">Back</span></a> }
+          { body && <div dangerouslySetInnerHTML={{__html: body }} /> }
       </div>
     </div>
   )
