2823fbe7c38e3524fa35f47ae8e813a37fa0ba4e
Andrew Shenstone <shenst1@gmail.com>
2021-03-11 15:36:26 -0800

    Add preview links.

--- components/layout.js
+++ components/layout.js
@@ -1,12 +1,15 @@
 import Link from 'next/link'
 import { useRouter } from 'next/router'
 import Head from "next/head"
-import {useEffect} from "react"
+import {useEffect, useState } from "react"
 import UIkit from "uikit"
 import Footer from "../components/footer"
 import NavigationNode from "../components/navigation-node"
+import {CopyToClipboard} from 'react-copy-to-clipboard';
 export default function Layout({ preview, children, settings, transparentNavigation = true }) {
   const router = useRouter()
+
+  const asPath = router.asPath.substring(1)
   useEffect(() => {
     const handleRouteChange = () => {
       UIkit.offcanvas('#mobile-navigation').hide()
@@ -16,6 +19,18 @@ export default function Layout({ preview, children, settings, transparentNavigat
       router.events.off('routeChangeStart', handleRouteChange)
     }
   }, [])
+  const [origin, setOrigin] = useState()
+  useEffect(() => {
+    setOrigin(window.location.origin)
+  })
+
+  const copyToClipboardSuccess = () => {
+    UIkit.notification({
+      message: `Preview link copied to clipboard!`,
+      pos: 'top-left',
+      timeout: 2000
+   });
+  }
  
   return (
     <>
@@ -25,11 +40,20 @@ export default function Layout({ preview, children, settings, transparentNavigat
       {/* uk-scrollspy="target: img:not(.nov-scrollspy-ignore); cls:uk-animation-fade; delay: 100" */}
       <div className="uk-offcanvas-content">
         <div className={`uk-navbar-container ${transparentNavigation && 'uk-sticky-remove-placeholder' }`} uk-sticky="">
-          {preview && <div className="uk-position-top-left uk-position-z-index"><a href="/api/exit-preview" className="uk-button uk-button-small uk-button-danger">Exit Preview Mode</a></div> }
+          {preview && 
+            <div className="uk-position-top-left uk-position-z-index">
+              <a href="/api/exit-preview" className="uk-button uk-button-small uk-button-danger">Exit Preview Mode</a>
+              <CopyToClipboard text={`${origin}/api/preview?secret=${process.env.NEXT_PUBLIC_PREVIEW_SECRET}&path=${asPath}`}
+                onCopy={copyToClipboardSuccess}>
+                <button className="uk-button uk-button-small uk-button-danger" style={{marginLeft: "3px" }}>
+                  <svg height="18" width="18" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8 5H6a2 2 0 00-2 2v12a2 2 0 002 2h10a2 2 0 002-2v-1M8 5a2 2 0 002 2h2a2 2 0 002-2M8 5a2 2 0 012-2h2a2 2 0 012 2m0 0h2a2 2 0 012 2v3m2 4H10m0 0l3-3m-3 3l3 3" />
+                  </svg>
+                </button>
+              </CopyToClipboard>
+            </div> }
           <nav className="uk-container" uk-navbar="mode: click;">
             <div className="uk-navbar-left">
-             
-              
               <ul className="uk-navbar-nav">
                 <li className="nav_hamburger">
                   <a href="#mobile-navigation" uk-toggle="">
--- pages/api/preview.js
+++ pages/api/preview.js
@@ -1,10 +1,10 @@
 export default async (req, res) => {
-  // Please set the NEXT_EXAMPLE_CMS_DATOCMS_PREVIEW_SECRET env variable
+  // Please set the NEXT_PUBLIC_PREVIEW_SECRET env variable
   // on Vercel/Netlify, or everyone will be able to enter Preview Mode and
   // see draft content!
 
   const secret =
-    process.env.NEXT_EXAMPLE_CMS_DATOCMS_PREVIEW_SECRET;
+    process.env.NEXT_PUBLIC_PREVIEW_SECRET;
 
   // Check the secret and next parameters
   if (secret && req.query.secret !== secret) {
@@ -15,6 +15,6 @@ export default async (req, res) => {
   res.setPreviewData({});
 
   // Redirect to the homepage
-  res.writeHead(307, { Location: "/" });
+  res.writeHead(307, { Location: `/${req.query.path ? req.query.path : ''}` });
   res.end();
 };
