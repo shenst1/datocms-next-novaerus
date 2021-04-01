118ce86b580a117c54ba98cfe0e43d510e6d94fd
Andrew Shenstone <shenst1@gmail.com>
2021-04-01 16:19:42 -0700

    Add locales for en-US and other.

--- next.config.js
+++ next.config.js
@@ -1,8 +1,13 @@
 require('dotenv').config()
 
+
 module.exports = {
   env: {
     NEXT_EXAMPLE_CMS_DATOCMS_API_TOKEN:
       process.env.NEXT_EXAMPLE_CMS_DATOCMS_API_TOKEN,
   },
+  i18n: {
+    locales: ['en-US', 'en-UK'], // en-UK could be anything. It is a placeholder for all other locales
+    defaultLocale: 'en-UK',
+  }
 }
