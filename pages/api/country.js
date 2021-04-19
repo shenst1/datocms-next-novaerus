e6e78ddc44dd62e30470b7e93638f30b5961a28c
Andrew Shenstone <shenst1@gmail.com>
2021-04-19 09:39:28 -0700

    Remove localhost logic on redirect.

--- pages/api/country.js
+++ pages/api/country.js
@@ -1,10 +1,8 @@
 export default async (req, res) => {
-  
+  // Geolocation redirect won't work locally because it will think the IP is the host
   const response = await fetch("https://telize-v1.p.rapidapi.com/location", {
     headers: {
-      ...(req.connection.remoteAddress !== "127.0.0.1" && {
-        "x-forwarded-for": req.headers['x-forwarded-for'] || req.connection.remoteAddress,
-      }),
+      "x-forwarded-for": req.headers['x-forwarded-for'] || req.connection.remoteAddress,
       "x-rapidapi-key": process.env.TELIZE_RAPID_API_KEY,
       "x-rapidapi-host": "telize-v1.p.rapidapi.com",
       "useQueryString": true
