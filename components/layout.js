adee3497a6b281cb2d7046f1016f0cb37d51801a
Andrew Shenstone <shenst1@gmail.com>
2021-06-04 16:19:53 -0700

    Add facebook and google tag manager.

--- components/layout.js
+++ components/layout.js
@@ -60,6 +60,13 @@ export default function Layout({ preview, children, settings, transparentNavigat
   return (
     <>
       <Head>
+      <meta name="facebook-domain-verification" content="x7y5dhn59oqtimsybb19u0q9e2rp2w" />
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <script dangerouslySetInnerHTML={{__html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','GTM-TQ5DKH3');`}}></script>
           <link rel="stylesheet" href="https://d1azc1qln24ryf.cloudfront.net/29147/NOV-Novaerus/style-cf.css?8jlnqz" />
       </Head>
       {/* uk-scrollspy="target: img:not(.nov-scrollspy-ignore); cls:uk-animation-fade; delay: 100" */}
--- pages/_document.js
+++ pages/_document.js
@@ -6,6 +6,9 @@ export default class MyDocument extends Document {
       <Html lang="en">
         <Head />
         <body>
+ 
+          <noscript dangerouslySetInnerHTML={{__html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-TQ5DKH3"
+height="0" width="0" style="display:none;visibility:hidden"></iframe>`}}></noscript>
           <Main />
           <NextScript />
         </body>
